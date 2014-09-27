fe410c816a353c11db2b6b62f0ee6b8851da5e20
Bjoern Rennhak <bjoern@clothesnetwork.com>
2014-09-27 04:59:41 +0100

    - All query command

---
+++ lib/fox/interface/thor/all.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+
+# System include
+require "thor"
+
+# Custom include
+require File.expand_path( File.dirname( __FILE__ ) + '/mixin/logger' )
+require File.expand_path( File.dirname( __FILE__ ) + '/mixin/database' )
+
+# @class      class All
+# @brief      drop structure from Hydra Service
+class All < Thor
+
+  # Include various partials
+  include ::Mixin::Logger
+  include ::Mixin::Database
+
+  default_task :all
+
+  # @fn         def all {{{
+  desc "all", "Get all Models"
+  option :force, :aliases => :f
+  def all
+    @logger.message :info, "Running Fox All command"
+    all_models.sort_by(&:name).each do |model|
+      puts "#{model.name} #{model.version} #{model.uuid}" 
+    end  
+
+  end # def all }}}
+
+end # of class All
+
+# vim:ts=2:tw=100:wm=100:syntax=ruby
